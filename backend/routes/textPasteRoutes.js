a9be31d02518ac5e75817eb6c77cf275fc3db4fb
f4kkey <161198439+f4kkey@users.noreply.github.com>
2025-11-19 20:34:15 +0700

    exposure

--- backend/routes/textPasteRoutes.js
+++ backend/routes/textPasteRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createPaste, getAllPastes, getPasteById, exportPaste, summarizePaste } from "../controllers/textPasteController.js";
+import { createPaste, getPublicPastes, getPasteById, exportPaste, summarizePaste } from "../controllers/textPasteController.js";
 import { authUser, optionalAuth } from "../middleware/authUser.js";
 
 const app = express.Router();
@@ -7,9 +7,9 @@ const app = express.Router();
 app.use(express.json());
 
 app.post('/', optionalAuth, createPaste);
-app.get('/', getAllPastes);
-app.get('/:id', getPasteById);
-app.get('/:id/export', exportPaste);
-app.get("/:id/summary", summarizePaste);
+app.get('/', getPublicPastes);
+app.get('/:id', optionalAuth, getPasteById);
+app.get('/:id/export', optionalAuth, exportPaste);
+app.get("/:id/summary", optionalAuth, summarizePaste);
 
 export default app;
