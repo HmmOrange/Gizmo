9c1a6e0aad88ed72f7e801f302eb24498a09060e
HmmOrange <leanhduy0206@gmail.com>
2025-11-22 15:47:49 +0700

    Add submodule for backend

--- backend/routes/authOAuth.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { redirectToMicrosoft, handleMicrosoftCallback } from "../auth/microsoftAuth.js";
-
-const router = express.Router();
-
-router.get("/microsoft", redirectToMicrosoft);
-router.get("/microsoft/callback", handleMicrosoftCallback);
-
-export default router;
